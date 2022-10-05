e69a11c6b6440f568306d88ccafa8f909529b62f
Abigail <fmsabigail54@gmail.com>
2022-10-05 15:49:57 +0100

    the calculating function

--- apps.js
+++ apps.js
@@ -1,18 +1,31 @@
-var form = document.getElementById('calculator')
+const inputValue = document.getElementById("value")
+const reference = document.getElementById("reference")
+const result = document.getElementById("result")
 
-
-form.addEventListener('submit', function(event){
-    event.preventDefault()
-    let value = document.getElementById('value').value
-})
-
-let result = document.getElementById("result").innerHTML
-
-
-form.addEventListener("click",Add)
- function Add(){
-    let final= result + value
-    console.log(final)
+function add(){
+    let calc = Number(result.innerText) + Number(inputValue.value)
+    result.innerText = calc
+    let message = 'added' + " " + inputValue.value
+    reference.innerText = message
 }
 
+function subtract(){
+    let calc = Number(result.innerText) - Number(inputValue.value)
+    result.innerText = calc
+    let message = 'subtracted' + " " + inputValue.value
+    reference.innerText = message
+}
 
+function multiplied(){
+    let calc = Number(result.innerText) * Number(inputValue.value)
+    result.innerText = calc
+    let message = 'multiplied by' + " " + inputValue.value
+    reference.innerText = message
+}
+
+function divide(){
+    let calc = Number(result.innerText) / Number(inputValue.value)
+    result.innerText = calc
+    let message = 'divided with' + " " + inputValue.value
+    reference.innerText = message
+}
